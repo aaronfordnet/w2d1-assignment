b081baa9cd8a365b4f8dab8480e417d134ac4a80
Aaron Ford <aaron@aaronford.net>
2018-07-09 18:51:24 +0000

    added step 3 file

---
+++ step3.js
@@ -0,0 +1,27 @@
+var https = require('https');
+
+function getAndPrintHTML (options) {
+
+  /* Add your code here */
+
+  https.get(options, function (response) {
+
+    response.setEncoding('utf8');
+
+    var buffer = '';
+
+    response.on('data', function (data) {
+      buffer += data;
+      console.log(buffer);
+    });
+
+  });
+
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML(requestOptions);
